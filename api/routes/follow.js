741bb53d0f34076faf0105a6832843fe1ae62a9a
Miguel Gonzalez <miguelglezcarlos@gmail.com>
2021-09-20 13:17:19 -0500

    sistema de follows -getFollowedUsers

--- api/routes/follow.js
+++ api/routes/follow.js
@@ -8,5 +8,6 @@ var md_auth = require('../middlewares/authenticated');
 api.post('/follow', md_auth.ensureAuth, FollowConroller.saveFollow);
 api.delete('/follow/:id', md_auth.ensureAuth, FollowConroller.unFollow);
 api.get('/following/:id?/:page?', md_auth.ensureAuth, FollowConroller.getFollowingUsers);
+api.get('/followed/:id?/:page?', md_auth.ensureAuth, FollowConroller.getFollewedUsers);
 
 module.exports = api;
