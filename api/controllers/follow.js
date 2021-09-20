ca657a31493cd8f4e03d453352290df7ebe80512
Miguel Gonzalez <miguelglezcarlos@gmail.com>
2021-09-20 12:56:35 -0500

    sistema de follows -getFollowingUsers

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -6,7 +6,6 @@ var mongoosePaginate = require('mongoose-pagination');
 
 var User = require('../models/user');
 var Follow = require('../models/follow');
-const follow = require('../models/follow');
 
 function saveFollow(req,res){
     var params = req.body;
@@ -15,7 +14,7 @@ function saveFollow(req,res){
     follow.user = req.user.sub;
     follow.followed = params.followed;
 
-    follow.save((err,followStored)=>{
+    Follow.save((err,followStored)=>{
         if(err) return res.status(500).send({message: 'error al guardar'});
         if(!followStored) return res.status(404).send({message: 'follow no se ha guardado'});
         return res.status(200).send({follow: followStored});
@@ -26,13 +25,38 @@ function unFollow(req,res){
     var userId = req.user.sub;
     var followId = req.params.id;
 
-    follow.find({'user':userId, 'followed':followId}).remove((err)=>{
+    Follow.find({'user':userId, 'followed':followId}).remove((err)=>{
         if(err) return res.status(500).send({message: 'error accion no disponible'});
         return res.status(200).send({message: 'follow eliminado'});
     })
 }
 
+function getFollowingUsers(req,res){
+    var userId = req.user.sub;
+
+    if(req.params.id && req.params.page){
+        userId = req.params.id;
+    }
+    var page = 1;
+    if(req.params.page){
+        page = req.params.page;
+    }else{
+        page = req.params.id;
+    }
+    var itemsPerPage = 4;
+    Follow.find({user:userId}).populate({path:'followed'}).paginate(page, itemsPerPage, (err, follows, total)=>{
+        if(err) return res.status(500).send({message: 'error en el servidor'});
+        if(!follows) return res.status(404).send({message:'no estas siguiendo ningun usuario'});
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total/itemsPerPage),
+            follows
+        })
+    });
+}
+
 module.exports = {
     saveFollow,
-    unFollow
+    unFollow,
+    getFollowingUsers
 }
--- api/routes/follow.js
+++ api/routes/follow.js
@@ -7,5 +7,6 @@ var md_auth = require('../middlewares/authenticated');
 
 api.post('/follow', md_auth.ensureAuth, FollowConroller.saveFollow);
 api.delete('/follow/:id', md_auth.ensureAuth, FollowConroller.unFollow);
+api.get('/following/:id?/:page?', md_auth.ensureAuth, FollowConroller.getFollowingUsers);
 
 module.exports = api;
