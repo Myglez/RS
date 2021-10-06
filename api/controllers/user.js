891e4926edb14f9a39290febcc22e7fc47f499cf
Miguel Gonzalez <miguelglezcarlos@gmail.com>
2021-10-05 23:33:01 -0500

    contador de publicaciones en getFollowCount

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -6,7 +6,8 @@ var fs = require('fs');
 var path = require('path');
 
 var User = require('../models/user');
-var Follow = require('../models/follow')
+var Follow = require('../models/follow');
+var Publication = require('../models/publication');
 var jwt = require('../services/jwt');
 
 
@@ -220,9 +221,16 @@ async function getFollowCount(user_id){
         return handleError(err);
     });
 
+    var publications = await Publication.count({"user":user_id}).exec().then((count)=>{
+        return count;
+    }).catch((err) => {
+        return handleError(err);
+    });
+
     return {
         following: following,
         followers: followed,
+        publications: publications
     }
 
 }
